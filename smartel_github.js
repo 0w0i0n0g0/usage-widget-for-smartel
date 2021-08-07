a5d59e00ea38e4495ce4db43557615de7634e4e4
0w0i0n0g0 <83498372+0w0i0n0g0@users.noreply.github.com>
2021-08-07 15:30:09 +0900

    use encodeURI to POST, add design

--- smartel_github.js
+++ smartel_github.js
@@ -1,3 +1,10 @@
+//set login info
+let hp_no = ""
+let user_nm = ""
+let pwd = ""
+
+
+
 //login to smartel
 let reqlogin = new Request("https://www.smartelmobile.com:5009/mobile2/m_login_proc.asp")
 
@@ -8,8 +15,8 @@ reqlogin.headers = {
     "Origin": "https://www.smartelmobile.com:5009"
 };
 
-//reqlogin.body = 'goUrl=' + '&hp_no=' + encodeURIComponent(hp_no) + '&user_nm=' + encodeURIComponent(user_nm) + '&pwd=' + encodeURIComponent(pwd)
-reqlogin.body = "POST data"
+reqlogin.body = 'goUrl=' + '&hp_no=' + hp_no + '&user_nm=' + encodeURI(user_nm) + '&pwd=' + encodeURI(pwd)
+
 reqlogin.onRedirect = function (request)
 	{
 		return null;
@@ -17,14 +24,14 @@ reqlogin.onRedirect = function (request)
 
 let reslogin = await reqlogin.loadString()
 
-//console.log(reslogin)
+
 
 //load usage
 let req = new Request("https://www.smartelmobile.com:5009/mobile2/m_realTimePay_info_lg.asp")
 
 let res = await req.loadString()
 
-//console.log(res)
+
 
 //extract usage
 let tbody = res.indexOf("<tbody>")
@@ -38,33 +45,46 @@ usage = usage.replace(/<\/td>/g,"");
 usage = usage.replace(/<tr bgcolor='#FFFFFF'>/g,"");
 usage = usage.replace(/<\/tr>/g,"");
 
-
 usage.replace(/\t/g, '');
 let usage_array = usage.split("\n");
 
 console.log(usage)
 
 
+
+//extracted usage
 let message =  parseInt(usage_array[3])
 let message_left =  parseInt(usage_array[4])
 
 let call =  parseInt(usage_array[10])
+call = (call/60).toFixed(0)
 let call_left =  parseInt(usage_array[11])
+call_left = (call_left/60).toFixed(0)
 
 let data =  parseInt(usage_array[17])
 data = (data/1000000).toFixed(2)
 let data_left =  parseInt(usage_array[18])
 data_left = (data_left/1000000).toFixed(2)
 
+
+
+//widget setting
 let widget = new ListWidget()
+
 let txt1 = widget.addText("문자  (건)")
-let txt1_1 = widget.addText(message + " / " + message_left)
-let txt2 = widget.addText("전화  (초)")
-let txt2_1 = widget.addText(call + " / " + call_left)
+let txt1_1 = widget.addText(message_left + " / " + message)
+let txt2 = widget.addText("전화  (분)")
+let txt2_1 = widget.addText(call_left + " / " + call)
 let txt3 = widget.addText("데이터  (GB)")
-let txt3_1 = widget.addText(data + " / " + data_left)
+let txt3_1 = widget.addText(data_left + " / " + data)
 
 txt1.font = Font.footnote()
+txt2.font = Font.footnote()
+txt3.font = Font.footnote()
+
+txt1_1.font = Font.boldSystemFont(25)
+txt2_1.font = Font.boldSystemFont(25)
+txt3_1.font = Font.boldSystemFont(25)
 
 widget.refreshAfterDate = new Date(Date.now() + 1000 * 120) 
 Script.setWidget(widget)
