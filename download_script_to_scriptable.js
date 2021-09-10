b17a566389e3a83738d93f26fc9b2ae360c06a94
0w0i0n0g0 <83498372+0w0i0n0g0@users.noreply.github.com>
2021-09-10 11:00:12 +0900

    url changed

--- download_script_to_scriptable.js
+++ download_script_to_scriptable.js
@@ -1,4 +1,4 @@
 var fm = FileManager.iCloud()
-var code = await new Request("https://han.gl/PadZQ").loadString()
+var code = await new Request("https://han.gl/fTNCc").loadString()
 fm.writeString(fm.joinPath(fm.documentsDirectory(), "smartel_By_0w0i0n0g0.js"), code)
 return 0
