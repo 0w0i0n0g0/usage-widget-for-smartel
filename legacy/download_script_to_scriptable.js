9a8ce9bfe204f04c2d251b4adeada94e41dabb72
0w0i0n0g0 <83498372+0w0i0n0g0@users.noreply.github.com>
2022-04-05 00:03:44 +0900

    refactor : Move to legacy.

--- legacy/download_script_to_scriptable.js
+++ legacy/download_script_to_scriptable.js
@@ -1,4 +1,3 @@
 var fm = FileManager.iCloud()
 var code = await new Request("https://han.gl/fTNCc").loadString()
 fm.writeString(fm.joinPath(fm.documentsDirectory(), "smartel_By_0w0i0n0g0.js"), code)
-return 0
