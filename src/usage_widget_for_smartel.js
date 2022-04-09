393ece5c42e59e30ff90dad0571217593da0c2ae
0w0i0n0g0 <83498372+0w0i0n0g0@users.noreply.github.com>
2022-04-09 10:16:55 +0900

    feat : Add hosted script link.

--- src/usage_widget_for_smartel.js
+++ src/usage_widget_for_smartel.js
@@ -5,6 +5,6 @@ const your_password = ""
 
 
 /*Don't touch here!*/
-var code = await new Request("").loadString()
+let code = await new Request("https://raw.githubusercontent.com/0w0i0n0g0/usage_widget_for_smartel/main/src/usage_widget_for_smartel_script.js").loadString()
 eval("async function fn(hp_no, user_nm, pwd) {" + code + "}")
 await fn(your_phone_number, your_name, your_password)
