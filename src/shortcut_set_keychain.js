e30b65b32c10465673dcb8f589d1334aa24fcf2c
0w0i0n0g0 <83498372+0w0i0n0g0@users.noreply.github.com>
2022-04-11 20:16:54 +0900

    feat : Use keychain.

---
+++ src/shortcut_set_keychain.js
@@ -0,0 +1,14 @@
+let NAME = "user_nm"
+let PHONE = "hp_no"
+let PASSWORD = "pwd"
+
+Keychain.set(NAME, args.plainTexts[0].replace(/ /gi, ''))
+Keychain.set(PHONE, args.plainTexts[1].replace(/ /gi, ''))
+Keychain.set(PASSWORD, args.plainTexts[2].replace(/ /gi, ''))
+
+if(Keychain.contains(NAME) && Keychain.contains(PHONE) && Keychain.contains(PASSWORD))
+{
+    return "성공!"
+} else {
+    return "실패. 다시 시도해주세요."
+}
